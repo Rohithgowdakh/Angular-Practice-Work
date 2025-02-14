583db6e24c6d668b0289c98b126a337f8aa6fb33
Rohith K H <rohithk2020@gmail.com>
2025-02-14 15:45:46 +0530

    Sample Wealth Track Angular Application

--- SIP_Calculator/src/app/app.routes.ts
+++ SIP_Calculator/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { SipCalculatorComponent } from './sip-calculator/sip-calculator.componen
 import { FdCalculatorComponent } from './fd-calculator/fd-calculator.component';
 import { StockDataComponent } from './stock-data/stock-data.component';
 import { IndianIndicesComponent } from './indian-indices/indian-indices.component';
+import { ExploreAllFundsComponent } from './explore-all-funds/explore-all-funds.component';
 export const routes: Routes = [
     {path:'', redirectTo:"/sip-calculator",pathMatch:'full'},
     {path:'sip-calculator',component:SipCalculatorComponent},
     {path:'fd-calculator',component:FdCalculatorComponent},
     {path:'stocks-data',component:StockDataComponent},
-    {path:'indian-indices',component:IndianIndicesComponent}
+    {path:'indian-indices',component:IndianIndicesComponent},
+    {path:'funds-data',component:ExploreAllFundsComponent}
 ];
